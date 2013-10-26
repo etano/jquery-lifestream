aff12be2dbc702de7cad8ece181e06f6699a358b
Ethan Brown <ethan.w.brown@gmail.com>
2013-10-26 22:49:30 +0000

    Compiled changes for adding pocket

--- jquery.lifestream.js
+++ jquery.lifestream.js
@@ -1267,21 +1267,13 @@ $.fn.lifestream.feeds.mendeley = function( config, callback ) {
       j = list.length;
       for( ; i<j; i++) {
         var item = list[i];
-        if(item.link.charAt(0)=='/') {
-          output.push({
-            date: new Date(item.pubDate),
-            config: config,
-            url: 'http://mendeley.com/groups/' + config.user,
-            html: $.tmpl( template.flagged1, item ),
-          });
-        } else {
-          output.push({
-            date: new Date(item.pubDate),
-            config: config,
-            url: 'http://mendeley.com/groups/' + config.user,
-            html: $.tmpl( template.flagged2, item ),
-          });
-        }
+        var tmplt = ( (item.link.charAt(0) === '/') ? template.flagged1 : template.flagged2 );
+        output.push({
+          date: new Date(item.pubDate),
+          config: config,
+          url: 'http://mendeley.com/groups/' + config.user,
+          html: $.tmpl( tmplt, item ),
+        });
       }
     }
     return output;
@@ -1439,6 +1431,53 @@ $.fn.lifestream.feeds.pinboard = function( config, callback ) {
 
 };
 })(jQuery);(function($) {
+$.fn.lifestream.feeds.pocket = function( config, callback ) {
+
+  var template = $.extend({},
+    {
+      pocketed: 'pocketed <a href="${link}">${title}</a>'
+    },
+    config.template),
+
+  parsePocket = function( input ) {
+    var output = [], list, i = 0, j;
+
+    if(input.query && input.query.results) {
+      list = input.query.results.rss.channel.item;
+      j = list.length;
+      for( ; i<j; i++) {
+        var item = list[i];
+        var tmplt = template.pocketed;
+        output.push({
+          date: new Date(item.pubDate),
+          config: config,
+          url: 'http://getpocket.com',
+          html: $.tmpl( tmplt, item ),
+        });
+      }
+    }
+    return output;
+  };
+
+  $.ajax({
+    url: $.fn.lifestream.createYqlUrl('select * from xml where url='
+      + '"http://www.getpocket.com/users/'
+      + config.user + '/feed/all/"'),
+    dataType: 'json',
+    success: function( data ) {
+      callback(parsePocket(data));
+    }
+  });
+
+  // Expose the template.
+  // We use this to check which templates are available
+  return {
+    "template" : template
+  };
+
+};
+})(jQuery);
+(function($) {
 $.fn.lifestream.feeds.posterous = function( config, callback ) {
 
   var template = $.extend({},
